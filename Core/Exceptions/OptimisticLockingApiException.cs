e011eb6cdef3088beea0e3acb5f9fc847fbafd34
Reinhard Holzner <reinhard@smint.io>
2018-09-18 21:46:57 +0200

    * Add optimistic version control exceptions * Fix empty return body in generated controllers

---
+++ Core/Exceptions/OptimisticLockingApiException.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ReinhardHolzner.Core.Exceptions
+{
+    public class OptimisticLockingApiException : ApiException
+    {
+        private string _errorCode;
+
+        public const string AccountConfigurationOptimisticLockViolated = "account_configuration_optimistic_lock_violated";        
+
+        public OptimisticLockingApiException(string errorCode, string message) : 
+            base(message)
+        {
+            _errorCode = errorCode;            
+        }
+       
+        public override int GetStatusCode()
+        {
+            return StatusCodes.Status409Conflict;
+        }
+
+        public override string GetErrorCode()
+        {
+            return _errorCode;
+        }
+
+        public override object GetObject()
+        {
+            return null;
+        }
+    }
+}
