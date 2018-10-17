0f00c83968121fa23745210f565ccf2c287ac238
Reinhard Holzner <reinhard@smint.io>
2018-10-17 10:47:52 +0200

    * Add missing import

--- HCore-Identity-PagesUI-Classes/Pages/Login.cshtml.cs
+++ HCore-Identity-PagesUI-Classes/Pages/Login.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using HCore.Identity.Attributes;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HCore.Identity.PagesUI.Classes.Pages
 {
