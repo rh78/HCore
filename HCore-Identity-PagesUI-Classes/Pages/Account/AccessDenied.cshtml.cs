79e9ccdfe776f6e1c8734ce72cbe8986ed113401
Reinhard Holzner <reinhard@smint.io>
2018-10-18 18:30:06 +0200

    * Add access denied page to default identity UI * Add user extension method to easily get the email address of the user

--- HCore-Identity-PagesUI-Classes/Pages/Account/AccessDenied.cshtml.cs
+++ HCore-Identity-PagesUI-Classes/Pages/Account/AccessDenied.cshtml.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using HCore.Identity.Attributes;
 
 namespace HCore.Identity.PagesUI.Classes.Pages.Account
 {
     [SecurityHeaders]
-    public class AccessDeniedModel : PageModel
+    public class ResetPasswordConfirmationModel : PageModel
     {
         public void OnGet()
         {
---
+++ HCore-Identity/Extensions/UserExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace System.Security.Claims
+{
+    public static class UserExtensions
+    {        
+        public static string GetEmailAddress(this ClaimsPrincipal user)
+        {
+            var emailAddressClaim = user.Claims.FirstOrDefault(claim => claim.Type == "email" || claim.Type == ClaimTypes.Email);
+
+            string emailAddress = emailAddressClaim?.Value;
+
+            if (string.IsNullOrEmpty(emailAddress))
+                return null;
+
+            return emailAddress;
+        }    
+    }
+}
