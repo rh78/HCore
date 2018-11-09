410f683c7fb051c04a8305ad798550b2d9afb392
Reinhard Holzner <reinhard@smint.io>
2018-11-09 23:06:08 +0100

    * Refactoring

---
+++ HCore-Amqp/Message/AMQPMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace HCore.Amqp.Message
+{
+    public abstract class AMQPMessage
+    {
+        public string Action { get; private set; }
+        
+        public AMQPMessage(string action)
+        {
+            Action = action;
+        }
+    }
+}
---
+++ HCore-Amqp/Processor/IAMQPMessageProcessor.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace HCore.Amqp.Processor
+{
+    public interface IAMQPMessageProcessor
+    {
+        Task<bool> ProcessMessageAsync(string address, string messageBodyJson);
+    }
+}
