ff23ac72ab619e76f586c458010a675c72054949
Reinhard Holzner <reinhard@smint.io>
2018-11-12 15:13:05 +0100

    * Add paging result helper for ElasticSearch

---
+++ HCore-Database/ElasticSearch/Helpers/PagingResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HCore.Database.ElasticSearch.Helpers
+{
+    public class PagingResult<TResult>
+    {
+        public List<TResult> Result { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+}
