bc3f68c39b220bd405a278f9fc21690e9c47aa7d
Reinhard Holzner <reinhard@smint.io>
2018-11-26 19:49:30 +0100

    * Fix pregenerating JSON default values if enums do not have correct serializer set

--- HCore-Web/Extensions/EnumExtensions.cs
+++ HCore-Web/Extensions/EnumExtensions.cs
@@ -1,12 +1,25 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace System
 {
     public static class EnumExtensions
     {
+        private static List<JsonConverter> converters = new JsonConverter[] { new StringEnumConverter() }.ToList();
+
+        private static JsonConverter StringEnumConverter()
+        {
+            throw new NotImplementedException();
+        }
+
         public static string ToEnumMemberAttrValue(this Enum e)
         {
-            string json = JsonConvert.SerializeObject(e);
+            string json = JsonConvert.SerializeObject(e, new JsonSerializerSettings()
+            {
+                Converters = converters
+            });
 
             json = json?.Replace("\"", "");
 
