9c6ce5d68f2cfee7463717888a946ed0ef4583b4
Reinhard Holzner <reinhard@smint.io>
2019-02-11 18:57:28 +0100

    * Add memcached logging

--- HCore-Cache/Cache/Impl/MemcachedCacheImpl.cs
+++ HCore-Cache/Cache/Impl/MemcachedCacheImpl.cs
@@ -1,4 +1,5 @@
 ﻿using Enyim.Caching;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -8,20 +9,29 @@ namespace HCore.Cache.Impl
     {
         private readonly IMemcachedClient _memcachedClient;
 
+        private readonly ILogger<MemcachedCacheImpl> _logger;
+
         // be careful, do not use Memcached 1.4.4 for Windows, it has wrong server time!
         // download 1.4.5!
         // https://blog.elijaa.org/2010/10/15/memcached-for-windows/
 
-        public MemcachedCacheImpl(IMemcachedClient memcachedClient)
+        public MemcachedCacheImpl(IMemcachedClient memcachedClient, ILogger<MemcachedCacheImpl> logger)
         {
             _memcachedClient = memcachedClient;
+
+            _logger = logger;
         }
 
         public async Task StoreAsync(string key, object value, TimeSpan expiresIn)
         {
             int cacheMinutes = (int)expiresIn.TotalMinutes;
 
-            await _memcachedClient.SetAsync(key, value, cacheMinutes).ConfigureAwait(false);
+            bool successfullyWritten = await _memcachedClient.SetAsync(key, value, cacheMinutes).ConfigureAwait(false);
+
+            if (!successfullyWritten)
+            {
+                _logger.LogWarning($"Value for key {key} could not be written to Memcached cache");
+            }
         }
         
         public async Task<T> GetAsync<T>(string key) where T : class
@@ -31,7 +41,12 @@ namespace HCore.Cache.Impl
 
         public async Task InvalidateAsync(string key)
         {
-            await _memcachedClient.RemoveAsync(key).ConfigureAwait(false);
+            bool successfullyRemoved = await _memcachedClient.RemoveAsync(key).ConfigureAwait(false);
+
+            if (!successfullyRemoved)
+            {
+                _logger.LogWarning($"Value for key {key} could not be removed from Memcached cache");
+            }
         }
     }
 }
