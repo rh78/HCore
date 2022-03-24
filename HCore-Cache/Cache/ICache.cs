99aad44eca918495f2e634b06226c857bab3c458
Yosif Velev <yosif@smint.io>
2022-03-24 13:03:38 +0200

    Update VIEApps.Enyim.Caching nuget package to latest working New GetAsync method that support multiple keys

--- HCore-Cache/Cache/ICache.cs
+++ HCore-Cache/Cache/ICache.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace HCore.Cache
@@ -6,10 +7,15 @@ namespace HCore.Cache
     public interface ICache
     {
         Task StoreAsync(string key, object value, TimeSpan expiresIn);
+
         Task<T> GetAsync<T>(string key) where T : class;
+
+        Task<IDictionary<string, T>> GetAsync<T>(IEnumerable<string> keys) where T : class;
+
         Task InvalidateAsync(string key);
         
         void Store(string key, object value, TimeSpan expiresIn);
+
         T Get<T>(string key) where T : class;
     }
 }
