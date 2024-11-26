5a36cf6977897290e0d68c69bf1b449beece535a
Reinhard Holzner <reinhard@smint.io>
2024-11-26 15:15:38 +0100

    Fix overrides

--- HCore-Web/Startup/Launcher.cs
+++ HCore-Web/Startup/Launcher.cs
@@ -167,14 +167,14 @@ namespace HCore.Web.Startup
 
                 config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
                       .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: false)
-                      .AddJsonFile($"appsettings.OEM.json", optional: true, reloadOnChange: false)
+                      .AddJsonFile("appsettings.OEM.json", optional: true, reloadOnChange: false)
                       .AddJsonFile($"appsettings.OEM.{env.EnvironmentName}.json", optional: true, reloadOnChange: false)
-                      .AddJsonFile($"appsettings.override.json", optional: true, reloadOnChange: false)
+                      .AddJsonFile("appsettings.override.json", optional: true, reloadOnChange: false)
                       .AddJsonFile($"appsettings.{env.EnvironmentName}.override.json", optional: true, reloadOnChange: false)
                       .AddJsonFile($"appsettings.{env.EnvironmentName}.local.json", optional: true, reloadOnChange: false)
-                      .AddJsonFile($"appsettings.OEM.local.json", optional: true, reloadOnChange: false)
+                      .AddJsonFile("appsettings.OEM.local.json", optional: true, reloadOnChange: false)
                       .AddJsonFile($"appsettings.OEM.{env.EnvironmentName}.local.json", optional: true, reloadOnChange: false)
-                      .AddJsonFile($"appsettings.override.local.json", optional: true, reloadOnChange: false)
+                      .AddJsonFile("appsettings.override.local.json", optional: true, reloadOnChange: false)
                       .AddJsonFile($"appsettings.{env.EnvironmentName}.override.local.json", optional: true, reloadOnChange: false);
 
                 if (env.IsDevelopment())
