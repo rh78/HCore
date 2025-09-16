26108bd32ed3f2fdea0676a6d03dba2d6194875d
Yosif Velev <yosif@smint.io>
2025-09-16 13:19:52 +0300

    Add Copyright statements

--- HCore-Database/Extensions/JTokenExtensions.cs
+++ HCore-Database/Extensions/JTokenExtensions.cs
@@ -1,4 +1,11 @@
-﻿using System.IO;
+﻿// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+// Original at https://github.com/elastic/elasticsearch-net/tree/7.17.5
+
+// Adjusted for HCore by Reinhard Holzner
+using System.IO;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
