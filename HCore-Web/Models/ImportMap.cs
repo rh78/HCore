167aed4ca6219c7a1505d9199149d70af2c80997
Reinhard Holzner <reinhard@smint.io>
2026-03-06 12:50:39 +0100

    Add script import map support

---
+++ HCore-Web/Models/ImportMap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace HCore.Web.Models
+{
+    internal class ImportMap
+    {
+        [JsonProperty("imports")]
+        public Dictionary<string, string> Imports { get; set; }
+    }
+}
