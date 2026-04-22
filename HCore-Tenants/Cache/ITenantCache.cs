22cb5e63841c06ccf298386eda69b2a8bffecd45
Yosif Velev <yosif@smint.io>
2026-04-22 10:36:26 +0300

    Rename isSiteAnalysis to isAnalysis

--- HCore-Tenants/Cache/ITenantCache.cs
+++ HCore-Tenants/Cache/ITenantCache.cs
@@ -6,11 +6,11 @@ namespace HCore.Tenants.Cache
 {
     public interface ITenantCache
     {
-        Task<ITenantInfo> GetTenantInfoByUuidAsync(long developerUuid, long tenantUuid, CultureInfo cultureInfo, bool isDraft, bool isSiteAnalysis);
-        Task CreateOrUpdateTenantInfoForUuidAsync(long developerUuid, long tenantUuid, CultureInfo cultureInfo, bool isDraft, bool isSiteAnalysis, ITenantInfo tenantInfo);
+        Task<ITenantInfo> GetTenantInfoByUuidAsync(long developerUuid, long tenantUuid, CultureInfo cultureInfo, bool isDraft, bool isAnalysis);
+        Task CreateOrUpdateTenantInfoForUuidAsync(long developerUuid, long tenantUuid, CultureInfo cultureInfo, bool isDraft, bool isAnalysis, ITenantInfo tenantInfo);
 
-        Task<ITenantInfo> GetTenantInfoBySubdomainLookupAsync(long developerUuid, string subDomainLookup, CultureInfo cultureInfo, bool isDraft, bool isSiteAnalysis);
-        Task CreateOrUpdateTenantInfoForSubdomainLookupAsync(long developerUuid, string subDomainLookup, CultureInfo cultureInfo, bool isDraft, bool isSiteAnalysis, ITenantInfo tenantInfo);
+        Task<ITenantInfo> GetTenantInfoBySubdomainLookupAsync(long developerUuid, string subDomainLookup, CultureInfo cultureInfo, bool isDraft, bool isAnalysis);
+        Task CreateOrUpdateTenantInfoForSubdomainLookupAsync(long developerUuid, string subDomainLookup, CultureInfo cultureInfo, bool isDraft, bool isAnalysis, ITenantInfo tenantInfo);
 
         Task InvalidateTenantInfosAsync(long developerUuid, long tenantUuid);
     }
