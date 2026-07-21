82d0849aa804912baf5ff1911c2fe73581adb664
Reinhard Holzner <reinhard@smint.io>
2026-07-21 17:03:07 +0200

    Remove Kickbox.NET

---
+++ HCore-Identity/Kickbox/Models/KickboxResponse.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+namespace HCore.Identity.Kickbox.Models
+{
+    public class KickboxResponse
+    {
+        [JsonProperty("result")]
+        public KickboxResultEnum Result { get; set; }
+
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+
+        [JsonProperty("role")]
+        public bool Role { get; set; }
+
+        [JsonProperty("free")]
+        public bool Free { get; set; }
+
+        [JsonProperty("disposable")]
+        public bool Disposable { get; set; }
+
+        [JsonProperty("accept_all")]
+        public bool AcceptAll { get; set; }
+
+        [JsonProperty("did_you_mean")]
+        public string DidYouMean { get; set; }
+
+        [JsonProperty("sendex")]
+        public double Sendex { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("user")]
+        public string User { get; set; }
+
+        [JsonProperty("domain")]
+        public string Domain { get; set; }
+
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("code")]
+        public int Code { get; set; }
+    }
+}
---
+++ HCore-Identity/Kickbox/Models/KickboxResultEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace HCore.Identity.Kickbox.Models
+{
+    public enum KickboxResultEnum
+    {
+        [EnumMember(Value = "deliverable")]
+        Deliverable,
+
+        [EnumMember(Value = "undeliverable")]
+        Undeliverable,
+
+        [EnumMember(Value = "risky")]
+        Risky,
+
+        [EnumMember(Value = "unknown")]
+        Unknown
+    }
+}
