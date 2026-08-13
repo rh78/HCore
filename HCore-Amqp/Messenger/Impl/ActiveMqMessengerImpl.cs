19d83c2988e78868d8542359246d5d301cd35dbf
Yosif Velev <yosif@smint.io>
2026-08-13 20:09:57 +0300

    Set prefetch policy to 50

--- HCore-Amqp/Messenger/Impl/ActiveMqMessengerImpl.cs
+++ HCore-Amqp/Messenger/Impl/ActiveMqMessengerImpl.cs
@@ -110,7 +110,7 @@ namespace HCore.Amqp.Messenger.Impl
                 }
             };
 
-            _connectionFactory.PrefetchPolicy.All = 1;
+            _connectionFactory.PrefetchPolicy.All = 50;
 
             _userName = Uri.UnescapeDataString(userInfoParts[0]);
             _password = Uri.UnescapeDataString(userInfoParts[1]);
