9b696ea7d9b9ec14f578688b043ef69882de1b88
Yosif Velev <yosif@smint.io>
2026-09-04 19:16:27 +0300

    Merge with development

--- HCore-Identity/Internal/OpenIddictApplicationManager.cs
+++ HCore-Identity/Internal/OpenIddictApplicationManager.cs
@@ -113,7 +113,7 @@ namespace HCore.Identity.Internal
 
         private const string WildcardCharacter = @"[a-zA-Z0-9\-]";
 
-        private const string AnyPortPattern = ":[0-9]{1,5}/";
+        private const string WildcardPortPattern = ":[0-9]{1,5}/";
 
         private bool IsUriMatch(string requestedUri, ICollection<string> allowedUris)
         {
@@ -135,7 +135,8 @@ namespace HCore.Identity.Internal
             }
 
             var regex = Regex.Escape($"{rule}")
-                        .Replace(@"WILDCARD", WildcardCharacter + "*");
+                .Replace(@"WILDCARD", WildcardCharacter + "*")
+                .Replace(":0/", WildcardPortPattern);
 
             return $"^{regex}";
         }
